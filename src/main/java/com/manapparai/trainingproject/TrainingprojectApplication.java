bfe415fe70607e2dd2356e660546b42e7c41ce40
raghu <raghumanbabu@gmail.com>
2023-10-07 11:09:20 +0200

    Added the first version of the training project.

---
+++ src/main/java/com/manapparai/trainingproject/TrainingprojectApplication.java
@@ -0,0 +1,15 @@
+package com.manapparai.trainingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingprojectApplication.class, args);
+		//Vetri change 1
+		//Lokesh change 1
+	}
+
+}
---
+++ src/test/java/com/manapparai/trainingproject/TrainingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manapparai.trainingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
